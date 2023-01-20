4b59546264d975d715a597c2c53a58f06cc852b6
amanSing-h <amansingh23181@gmail.com>
2023-01-20 20:55:18 +0530

    3rd
    Third

---
+++ Functions_Methods_04/src/VarArgs.java
@@ -0,0 +1,14 @@
+public class VarArgs { // variable length arguments
+    public static void main(String[] args) {
+        /*
+        when we create a method that takes a variable length of arguments
+        that is known as variable length arguments(VarArgs)
+
+        like when we do not know how many inputs we are going to give
+        e.g.,
+        static void fun(int...v){    // here v is variable name we can take anything
+        }
+
+        */
+    }
+}
