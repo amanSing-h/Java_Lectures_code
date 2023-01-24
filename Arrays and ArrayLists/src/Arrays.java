8b24163d1e0e5e8a3054fd888514ffc88df652f9
amanSing-h <amansingh23181@gmail.com>
2023-01-24 22:01:37 +0530

    4th
    Fourth

---
+++ Arrays and ArrayLists/src/Arrays.java
@@ -0,0 +1,38 @@
+public class Arrays {
+    public static void main(String[] args) {
+        // Q. store a roll number
+        int a = 19;
+
+        // Q. store a person's name
+        String name = "aman";
+
+        // Q. store 3 roll numbers
+        int rno1 = 23;
+        int rno2 = 24;
+        int rno3 = 35;
+
+        // Q. Now store 500 roll numbers
+        // so here creating variables and declaring again and again is very tedious
+        // so here comes Arrays
+
+        /*
+        syntax : datatype[] variable_name = new datatype[size]
+
+         */
+        // store 5 roll numbers
+        int[] rnos = new int[5];
+        // or directly
+        int[] rnos2 = {23, 12, 45, 32, 15};
+
+        /*
+        here datatype represents what is the type of data store inside the array
+        all the type of  data in the array should be same
+         means if array is type of int then all the data in the array should be int
+         or if string then string or if boolean then boolean and so on.
+
+         */
+
+        
+
+    }
+}
